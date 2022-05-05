10226f5565473b6ac9ffb307f99889494fffec49
dhq_boiler <dhq_boiler@live.jp>
2022-05-05 20:44:34 +0900

    organize using

--- Tenpai/Tenpai/Models/Tiles/Tile.cs
+++ Tenpai/Tenpai/Models/Tiles/Tile.cs
@@ -1,10 +1,6 @@
 ﻿using Reactive.Bindings;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using Tenpai.Converters;
 
